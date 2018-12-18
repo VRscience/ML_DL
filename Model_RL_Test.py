3c0d102fe871219598187d76ac583f29d239e45e
vrscience <vrscience>
2018-12-18 15:49:53 +0100

    Create Model_Saving and loading

--- Model_RL_Test.py
+++ Model_RL_Test.py
@@ -14,7 +14,6 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
@@ -55,48 +54,9 @@ validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-# Test options and evaluation metric
-seed = 7
-scoring = 'accuracy'
-
-# Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
-
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
-    
-# Compare Algorithms
-fig = plt.figure()
-fig.suptitle('Algorithm Comparison')
-ax = fig.add_subplot(221)
-plt.boxplot(results)
-ax.set_xticklabels(names)
-plt.show()
-
-# Make predictions on validation dataset
-knn = KNeighborsClassifier()
-knn.fit(X_train, Y_train)
-predictions = knn.predict(X_validation)
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
-
-#Save model
 directory = r"C:\Users\vrizzo\PycharmProjects\ML_DL\Saved_models" + r"\\".strip()
-filename = directory + ("saved_model.sav")
+filename = directory + "saved_model.sav"
 
-saved_model = joblib.dump(knn, open(filename, "wb"))
+loaded_model = joblib.load(filename)
+result = loaded_model.score(X_validation, Y_validation)
+print(result)
